d915b5a49a2aba6a336589164a7398095ce1d41f
Vladimir <vovabog071@gmail.com>
2025-10-14 12:17:57 +0800

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,12 +4,12 @@ module.exports = {
       name: 'django',
       script: 'C:\\Users\\vovab\\AppData\\Local\\Programs\\Python\\Python312\\python.exe',
       args: 'manage.py runserver 127.0.0.1:8000',
-      cwd: 'C:\\Users\\vovab\\Desktop\\Флешка\\4 курс (1 семестр)\\Основы DevOps\\DevOps_LAB1\\Django01_server',
+      cwd: 'C:\\Users\\vovab\\Desktop\\DevOps_LAB1\\Django01_server',
       interpreter: 'none'
     },
     {
       name: "vue",
-      cwd: "C:\\Users\\vovab\\Desktop\\Флешка\\4 курс (1 семестр)\\Основы DevOps\\DevOps_LAB1\\Django01_server\\client",
+      cwd: "C:\\Users\\vovab\\Desktop\\DevOps_LAB1\\Django01_server\\client",
       script: "cmd",
       args: "/c npm run dev",
       interpreter: "none"
